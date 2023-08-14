73f7bb32d272de4a94bff038568f5cbb88ebc532
madhav-1810 <142152369+madhav-1810@users.noreply.github.com>
2023-08-14 08:47:14 +0530

    Update p1-1_2.cpp

--- p1-1_2.cpp
+++ p1-1_2.cpp
@@ -4,7 +4,11 @@ using namespace std;
 int main()
 {
 	int a,b;
-	
+
+	cout<<"I am Madhav Vasani "<<endl;
+	cout<<"En_no    220130318118 "<<endl;
+	cout<<"---------------------"<<endl;
+
 	cout<<"Enter the number 1::";
 	cin>>a;
 	
